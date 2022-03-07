4bac428f7388a48134fc75533479fcd5570e508e
Juan David R <jdavidreyes09@gmail.com>
2022-03-06 23:13:38 -0500

    avance home hero slider

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ $(document).ready(function(){
 
     $('.button_nav').click(function(){
         $('nav.navbar').toggleClass('active');
+        $('.button_nav .fas').toggleClass('fa-times');
     });
 
     // FOOTER CLICK MOBILE
@@ -16,5 +17,24 @@ $(document).ready(function(){
         $(this).next().slideToggle();
         $(this).toggleClass('active_nav');
     });
+
+    // SLIDER HOME HERO
+
+    function HomeHero(){
+        let swiper = new Swiper(".homeHero_swiper", {
+            parallax: true,
+            effect: 'fade',
+            fadeEffect: {
+                crossFade: true
+            },
+            pagination: {
+              el: ".homeHero_swiper .swiper-pagination",
+            },
+        });
+    }
+
+
+    HomeHero();
+    
 
 });
