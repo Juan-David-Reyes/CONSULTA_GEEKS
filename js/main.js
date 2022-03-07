4d2143a6acd072d380af185ae25a478e483098b7
Juan David R <jdavidreyes09@gmail.com>
2022-03-06 19:33:14 -0500

    Estructura home OK

--- js/main.js
+++ js/main.js
@@ -1,19 +1,20 @@
 $(document).ready(function(){
 
+    // MENU MOBILE
+
+    $('.button_nav').click(function(){
+        $('nav.navbar').toggleClass('active');
+    });
+
     // FOOTER CLICK MOBILE
 
     $('#link_f1').click(function(){
         $(this).next().slideToggle();
         $(this).toggleClass('active_nav');
-    })
+    });
     $('#link_f2').click(function(){
         $(this).next().slideToggle();
         $(this).toggleClass('active_nav');
-    })
+    });
 
-    // MENU MOBILE
-
-    $('.button_nav').click(function(){
-        $('nav.navbar').toggleClass('active');
-    })
 });
