f43ef674c4c8206fcc829d2acf35cdf023181ba3
Juan David R <jdavidreyes09@gmail.com>
2022-03-07 23:03:14 -0500

    avance home secciones

--- js/main.js
+++ js/main.js
@@ -33,8 +33,39 @@ $(document).ready(function(){
         });
     }
 
+     function homeComoFunciona(){
+         let swiper = new Swiper(".swiperComoFunciona", {
+             spaceBetween: 20,
+             slidesPerView: 1,   
+             pagination: {
+               el: ".swiperComoFunciona .swiper-pagination",
+             },
+             breakpoints: {
+                // when window width is >= 320px
+                320: {
+                  slidesPerView: 1,
+                  spaceBetween: 20
+                },
+                540: {
+                    slidesPerView: 2,
+                    spaceBetween: 20
+                  },
+                // when window width is >= 480px
+                768: {
+                  slidesPerView: 3,
+                  spaceBetween: 20
+                },
+                // when window width is >= 640px
+                1000: {
+                  slidesPerView: 4,
+                  spaceBetween: 20
+                }
+              }
+         });
+     }
+
 
     HomeHero();
-    
+     homeComoFunciona();
 
 });
